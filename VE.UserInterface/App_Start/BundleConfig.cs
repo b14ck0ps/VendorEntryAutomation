41d57de7ac131603e9eb690ab63dd318ce3644f2
Azran Hossain <mr69751@gmail.com>
2024-01-28 22:45:19 +0600

    datatableadded

--- VE.UserInterface/App_Start/BundleConfig.cs
+++ VE.UserInterface/App_Start/BundleConfig.cs
@@ -19,12 +19,11 @@ namespace VE.UserInterface
                 "~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js"));
+                "~/Scripts/bootstrap.js", "~/Scripts/jquery.dataTables.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
-                "~/Content/site.css","~/Content/Loader.css"
-                ));
+                "~/Content/site.css", "~/Content/Loader.css", "~/Content/jquery.dataTables.min.css"));
         }
     }
 }
