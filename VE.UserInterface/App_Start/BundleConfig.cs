67203cacf5219652bf1c4701c5aa1cb7008fb968
Azran Hossain <mr69751@gmail.com>
2024-01-28 22:23:09 +0600

    veryyyy big loader added on submit

--- VE.UserInterface/App_Start/BundleConfig.cs
+++ VE.UserInterface/App_Start/BundleConfig.cs
@@ -23,7 +23,8 @@ namespace VE.UserInterface
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
-                "~/Content/site.css"));
+                "~/Content/site.css","~/Content/Loader.css"
+                ));
         }
     }
 }
